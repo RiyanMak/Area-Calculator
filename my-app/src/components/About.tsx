9ae053cbb225027da0d3cfe517ff81a0cecc90de
“quac88” <mac2@thrasher.com>
2023-12-22 17:21:37 -0500

    feat: create a first compiled react app build

---
+++ my-app/src/components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css'; // Importing CSS
+
+const About: React.FC = () => {
+  return (
+    <section id="about" className="about">
+      <h2>ABOUT SECTION</h2>
+      <p>SOME WORDS</p>
+      {/* Add your bio here */}
+    </section>
+  );
+};
+
+export default About;
